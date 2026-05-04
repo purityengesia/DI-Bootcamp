0de1e7c4d572351233cd8f3cbc860b9ebf04c806
purityengesia <purityengesia@gmail.com>
2026-05-04 13:46:36 +0300

    Delete DI_Bootcamp/routes,controllers and middleware directory

--- DI_Bootcamp/routes,controllers and middleware/xpexercise/exercise1/app.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const indexRouter = require('./routes/index');
-
-app.use('/', indexRouter);
-
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
-});
