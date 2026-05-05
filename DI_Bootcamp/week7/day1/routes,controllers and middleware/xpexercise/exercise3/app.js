15556d144a3bdea3be892c7aee8cd4e1f00093f5
purityengesia <purityengesia@gmail.com>
2026-05-05 12:56:08 +0300

    Delete DI_Bootcamp/week7/day1/routes,controllers and middleware directory

--- DI_Bootcamp/week7/day1/routes,controllers and middleware/xpexercise/exercise3/app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-const bookRouter = require('./routes/books');
-
-app.use(express.json());
-app.use('/books', bookRouter);
-
-app.listen(3000, () => console.log('Book API running on port 3000'));
